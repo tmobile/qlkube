065c9ab54ed262aa6d49927fa6b594c7ffd81396
Jeannette Tang <jeannette.tang8@t-mobile.com>
2021-05-26 18:45:07 +0000

    Dynamic subscriptions + Authorization

--- src/oas.js
+++ src/oas.js
@@ -1,26 +1,14 @@
-const got = require('got');
-const promiseAny = require('promise-any');
-const logger = require('pino')({useLevelLabels: true});
+const fetch = require('node-fetch');
 
-const openApiPaths = ['/openapi/v2', '/swagger.json'];
+const openApiPaths = ['/openapi/v2'];
 
-// execute parallel requests to possible open api endpoints and return first success
 module.exports = async function getOpenApiSpec(url, token) {
-    let gotProms = [];
-    for (let p of openApiPaths) {
-        const gotProm = got(p, {
-            baseUrl: url,
-            json: true,
-            timeout: 5 * 1000,
-            headers: {Authorization: `Bearer ${token}`},
-        }).then(r => {
-            logger.info({url, path: p}, "successfully retrieved open api spec from this path")
-            return r.body
-        }).catch(err => {
-            logger.info({cause: err.message, url, path: p}, "failed to retrieve open api spec from this path - will try another")
-            throw err
-        });
-        gotProms.push(gotProm)
-    }
-    return promiseAny(gotProms);
+  const openApiUrl = url + openApiPaths[0];
+  const response = await fetch(openApiUrl, {
+    timeout: 5000,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.json();
 };
---
+++ src/utilities.js
@@ -0,0 +1,60 @@
+/* eslint-disable */
+const Oas3Tools = require('openapi-to-graphql/lib/oas_3_tools');
+const preprocessor_1 = require('openapi-to-graphql/lib/preprocessor');
+const schema_builder_1 = require('openapi-to-graphql/lib/schema_builder');
+const getDefaultGraphQlSchemaOptions = require('../model/schemaDefaultOptions');
+
+function mapGraphQlDefaultPaths(spec) {
+  options = getDefaultGraphQlSchemaOptions();
+  return new Promise((resolve, reject) => {
+    Oas3Tools.getValidOAS3(spec)
+      .then((oas) => {
+        resolve(translateOpenAPIToGraphQLREV([oas], options));
+      })
+      .catch((err) => console.log('Error', err));
+  });
+}
+
+function mapK8ApiPaths(oas, pathNames, graphQlSchemaMap) {
+  return (mappedK8Paths = pathNames
+    .map((pathName) => {
+      let currentPath = oas['paths'][pathName];
+      let currentPathKeys = Object.keys(currentPath);
+      if (
+        currentPathKeys.length > 0 &&
+        oas['paths'][pathName][currentPathKeys[0]][
+          'x-kubernetes-group-version-kind'
+        ]
+      ) {
+        let tempK8Type =
+          oas['paths'][pathName][currentPathKeys[0]][
+            'x-kubernetes-group-version-kind'
+          ]['kind'];
+        return {
+          k8sUrl: pathName,
+          k8sType: tempK8Type,
+          schemaType: String(graphQlSchemaMap[pathName]),
+        };
+      } else {
+        return false;
+      }
+    })
+    .filter((obj) => obj !== false));
+}
+
+function translateOpenAPIToGraphQLREV(oass, options) {
+  const data = preprocessor_1.preprocessOas(oass, options);
+  let schemaTypeMap = {};
+  Object.entries(data.operations).forEach(([operationId, operation]) => {
+    const field = schema_builder_1.getGraphQLType({
+      def: operation.responseDefinition,
+      data,
+      operation,
+    });
+    schemaTypeMap[operation.path] = field;
+  });
+  return schemaTypeMap;
+}
+
+exports.mapGraphQlDefaultPaths = mapGraphQlDefaultPaths;
+exports.mapK8ApiPaths = mapK8ApiPaths;
