11f6164e4e15a09bb44ee25b46a0e2ab7d24988c
Detrich Utti <detrich.utti1@t-mobile.com>
2022-02-11 14:44:00 -0600

    feat: http request routed to queries, refactor, client disconnect handling, max events increased

--- src/index2.js
+++ src/index2.js
@@ -13,24 +13,21 @@ const {
 
 const utilities = require('./utilities');
 const getOpenApiSpec = require('./oas');
-const watch = require('./watch');
 const path = require('path');
 
 //------
-const app = express()
-const bodyParser = require('body-parser');
-const { createClient } = require('graphql-ws');
 const ws = require('ws'); // yarn add ws
-const { useServer }  = require('graphql-ws/lib/use/ws');
-var events = require('events');
-const Crypto = require('crypto');
-var process = require('process')
-const serverCache = require('./cache/serverGenCache');
-const pubsub = new PubSub();
 const wsServer = require('http').createServer();
 const WebSocketServer  = require('ws').Server; // yarn add ws
 const wss = new WebSocketServer({ server: wsServer });
-var process = require('process');
+const { useServer }  = require('graphql-ws/lib/use/ws');
+const { createClient } = require('graphql-ws');
+const bodyParser = require('body-parser');
+var events = require('events').EventEmitter.defaultMaxListeners = 100;;
+const Crypto = require('crypto');
+var process = require('process')
+const pubsub = new PubSub();
+const serverCache = require('./cache/serverGenCache');
 const { default: cluster } = require('cluster');
 //------
 
@@ -38,59 +35,33 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 const inCluster = process.env.IN_CLUSTER !== 'false';
 logger.info({ inCluster }, 'cluster mode configured');
 
-const _token = `eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik1yNS1BVWliZkJpaTdOZDFqQmViYXhib1hXMCJ9.eyJhdWQiOiI1YzllODJiNS03NzBiLTQ2NzYtYjZiOC0zNzU5ZDdiNGEwMmIiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vYmUwZjk4MGItZGQ5OS00YjE5LWJkN2ItYmM3MWEwOWIwMjZjL3YyLjAiLCJpYXQiOjE2NDQ1MTU3ODksIm5iZiI6MTY0NDUxNTc4OSwiZXhwIjoxNjQ0NTE5Njg5LCJlbWFpbCI6IkRldHJpY2guVXR0aTFAVC1Nb2JpbGUuY29tIiwiZ3JvdXBzIjpbIkF0ZXJuaXR5IFVzZXJzIiwiTGljZW5zZV9XaW4xMF9SU0FUIEZ1bGwiLCJQR0hOLUlTQS1Mb2FkX0JhbGFuY2VyLVBSRCIsIk1vYmlsZUlyb25fRU5UIiwiUm9sZV9UTVVTX0ZURSIsIkxpY2Vuc2VfTWljcm9zb2Z0IE9mZmljZSAzNjUgeDg2IE9uLURlbWFuZCIsIlNBIE5vdGlmaWNhdGlvbnMiLCJMaWNlbnNlX01pY3Jvc29mdCBBenVyZSBFTVMiLCJBdGVybml0eSBFVFMiLCJSb2xlX1RlY2hub2xvZ3lfQWxsIiwiUmVsaWFuY2VXb3JrTG9nIiwiTGljZW5zZV9NYWtlIE1lIEFkbWluIEVucm9sbG1lbnQiLCJTcGx1bmtWaWV3X0FsbFVzZXJzIiwiT1RfVGVjaG5vbG9neV9BbGwiLCJEU0dfV2ViYl9GVEUiLCJBcHBEaXN0X1JTQVQgRnVsbCIsIkFwcERpc3RfTWljcm9zb2Z0IE9mZmljZSAzNjUgeDg2IE9uLURlbWFuZCIsIkNpdHJpeF9DYXJlX1JlbW90ZUFjY2VzcyIsIlZQTi1OZXR3b3JrLUVJVCIsIk9LVEFfQXBwcmVjaWF0aW9uLVpvbmUiLCJBbGxvdyBXb3Jrc3RhdGlvbiBFbGV2YXRpb24iLCJUZWNobm9sb2d5X1JORVx1MDAyNk9fMiIsIk9UX0ZURSIsIk1JX0FwcHNfVE5hdGlvbl9XcmFwcGVkIl0sIm5hbWUiOiJVdHRpLCBEZXRyaWNoIiwibm9uY2UiOiIzZWViZDNjYy01NDIxLTQ3ODctODAzNC05ZjBlYjgwOThiN2EiLCJvaWQiOiIxN2Q5MjliMi02OTA2LTQ3ZTAtOGNhOS0zYWJiMzk3MmJjN2IiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJEZXRyaWNoLlV0dGkxQFQtTW9iaWxlLmNvbSIsInJoIjoiMC5BUk1BQzVnUHZwbmRHVXU5ZTd4eG9Kc0NiTFdDbmx3TGQzWkd0cmczV2RlMG9Dc1RBTjAuIiwic3ViIjoiMlFQeWN6QWtwUDJodFJQRXBTWV8tVHlKOVk2NndaNzZWNDRtbHpNWk5iUSIsInRpZCI6ImJlMGY5ODBiLWRkOTktNGIxOS1iZDdiLWJjNzFhMDliMDI2YyIsInV0aSI6InNpdmdKN3B1OWstOVVydTVocVFtQUEiLCJ2ZXIiOiIyLjAiLCJzYW1hY2NvdW50bmFtZSI6ImR1dHRpMSJ9.JW5lMrfmIazw86dPbzWJqhbkcqzcob3Zitd3MOgBl94OnUcslaiYm3E776Mhp9nP2hIIAU1wKd44t3BZNtjC63M-LrqOiB3YCltzL0HtagQdWIrZ5Jpg197hLTv4SM-ZxfIV7qs2SCYVo57W0oU8l6y41gR18Zduabn-g2qNCxeLOZEob5xY9otp29bMjHobrp15yByA4OBGhslfnCuYa0SbYhMHlksAghZPdGJ8DuTQHhmzUSdzdILtORECmvqgIGbyzHa_lNAJOCnY9rtbzVG9yCf7VexcfcGnSzSUyKNyfRWMmzG71EJ0Sh7AKXtYi1RlcLeKxeDOZXnJCt9xPQ`
-
-app.use(bodyParser.json());
-
-// TEST QUERY
-app.get('/gql', async(req, res) => {
-  console.log(req?.body)
-  const reqQuery= `query {
-    listAppsV1NamespacedDeployment(namespace: "css-dev-duck-dev-w2") {
-      items {
-        metadata{
-          name
-          namespace
-          labels
-        }
-        spec {
-          selector {
-            matchLabels
-          }
-        }
-      }
-    }
-  }`;
-  const _clusterUrl= 'https://west.dev.duck.master.kube.t-mobile.com:6443'
-  const connectionParams= {
-    authorization: `Bearer ${_token}`,
-    clusterUrl: _clusterUrl,
-    clientId: Date.now().toString(36) + Math.random().toString(36).substr(2),
-  }
-
-  res.send({
-    data : await gqlServerRouter(
-      null,
-      _clusterUrl,
-      null,
-      null,
-      _token,
-      reqQuery,
-      connectionParams,
-      requestTypeEnum.query
-    ) 
-  })
-});
-
-app.post('/add', (req, res) => {
-    console.log(req?.body?.testpath)
-    res.send('test');
-});
-
-
 let clientInternalWsMap={};
 let internalSubObjMap={}
 let clientToInternalSubIdMap={};
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// GQL QUERIES
+app.get('/gql', async(req, res) => {
+  if(req?.headers?.connectionparams){
+    const queryParams= JSON.parse(req?.headers?.connectionparams);
+    const { query, authorization, clusterUrl }= queryParams;
+    res.send({
+      data : await gqlServerRouter(
+        null,
+        clusterUrl,
+        null,
+        null,
+        authorization,
+        query,
+        queryParams,
+        requestTypeEnum.query
+      ) 
+    })
+  }
+});
 
 const requestTypeEnum={
   subscribe: 'subscribe',
@@ -140,7 +111,6 @@ const pairSubToClient = (clientId, subObj, internalSubId, clientSubId) => {
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(data) {
     try {
-
       const connectionMessage= JSON.parse(data);
       const { requestType, clientId, query, connectionParams }= connectionMessage;
 
@@ -148,7 +118,13 @@ wss.on('connection', function connection(ws) {
       if(requestType === requestTypeEnum.subscribe){
         ws.clientId= clientId;
         logger.debug('Internal ws subscribe request from', connectionParams.clientId)
-        if(connectionParams?.clusterUrl&&clientId){  
+        if(
+          connectionParams?.clusterUrl&&
+          clientId&&
+          emitterId&&
+          connectionParams?.authorization&&
+          query
+        ){  
           const emitterId= connectionParams.clientId;
           gqlServerRouter(
             emitterId, 
@@ -160,12 +136,13 @@ wss.on('connection', function connection(ws) {
             connectionParams,
             requestTypeEnum.subscribe
           )
+        }else{
+          ws.send('error', 'Invalid request')
         }
       }
 
       // END INTERNAL SOCKET
       else if(requestType === requestTypeEnum.close){
-        logger.debug('Internal ws close request from', connectionParams.clientId)
         destroySpeifiedInternalWebsocket(connectionParams.clientSubId, connectionParams.clientId);
       }
     } catch (error) {
@@ -181,12 +158,9 @@ wss.on('connection', function connection(ws) {
   ws.on('close', () => {
     try {
       // End all internal websockets for disconnected client
-      logger.debug('Meta websocket closed for', ws.clientId)
-      for(let cachedInternalSubId of clientInternalWsMap[ws.clientId]){
-        internalSubObjMap[cachedInternalSubId].dispose();
-        delete internalSubObjMap[cachedInternalSubId];
-      };
-      delete clientInternalWsMap[ws.clientId]
+      logger.debug('Meta websocket closed for', ws.clientId);
+      const currentClientId= ws.clientId;
+      destroyCachedDataForClient(currentClientId);
     } catch (error) {
       return {
         error: {
@@ -210,6 +184,25 @@ const destroySpeifiedInternalWebsocket = (clientSubId, clientId) => {
   clientInternalWsMap[clientId]= filteredClientInternalWs;
 }
 
+// ENDS ALL CACHED ROUTING DATA FOR CLIENT
+const destroyCachedDataForClient = (wsId) => {
+  logger.debug('Internal ws close request from', wsId)
+  let internalSubsForClient=[];
+  for(let cachedInternalSubId of clientInternalWsMap[wsId]){
+    internalSubObjMap[cachedInternalSubId].dispose();
+    delete internalSubObjMap[cachedInternalSubId];
+    internalSubsForClient.push(cachedInternalSubId);
+  };
+  delete clientInternalWsMap[wsId];
+  let newClientToInternalSubIdMap= {...clientToInternalSubIdMap}
+  for(let internalSubClientKey of Object.keys(clientToInternalSubIdMap)){
+    if(internalSubsForClient.includes(clientToInternalSubIdMap[internalSubClientKey])){
+      delete newClientToInternalSubIdMap[internalSubClientKey]
+    }
+  }
+  clientToInternalSubIdMap= newClientToInternalSubIdMap;
+}
+
 // DETERMINES IF NEW GQL SERVER NEEDS GENERATION
 const gqlServerRouter = async(
   emitterId, 
@@ -225,8 +218,8 @@ const gqlServerRouter = async(
     logger.debug('Generating server for', clusterUrl)
     const { serverUrl, serverObj, error }= await generateGqlServer(freePort, clusterUrl, token);
     if(!error){
+      serverCache.cacheServer(null, clusterUrl, serverUrl, freePort, serverObj);
       if(requestMode === requestTypeEnum.subscribe){
-        serverCache.cacheServer(null, clusterUrl, serverUrl, freePort, serverObj);
         const emitter = connectSub(serverUrl, emitterId, clientId, query, connectionParams);
         serverCache.movePortUsed(freePort);
         emitter.on(emitterId, data => {
@@ -243,7 +236,6 @@ const gqlServerRouter = async(
   }
 
   const gqlServerData= serverCache.getServer(clusterUrl);
-
   // SERVER FOR CLUSTER EXISTS
   if(gqlServerData){
     const { gqlServerUrl, gqlServerClient }= gqlServerData;
@@ -262,13 +254,11 @@ const gqlServerRouter = async(
       const queryResponse= await connectQuery(gqlServerUrl, query, connectionParams);
       return queryResponse;
     }
-
   }
 
   // SERVER DOSENT EXIST -> GENERATE
   else{
     logger.debug('Check if ports are available....', serverCache.portQueue);
-
     const freePort= serverCache.getPort();
 
     // FREE PORT -> CREWATE SERVER AT PORT
@@ -298,18 +288,6 @@ const gqlServerRouter = async(
     }
   }
 }
-
-// BASE SERVER STARTUP
-const serverStart = () => {
-  let PORT = `${process.env.SERVER_PORT}`;
-  if (!process.env.SERVER_PORT) {
-    PORT = 8080;
-  }
-  wsServer.on('request', app);
-  wsServer.listen(PORT, () => {
-    console.log('Ws server Listening on ' + wsServer.address().port)
-  }) 
-};
 
 // GENERATES CLUSTER SCHEMA FOR NEW SERVERS
 const generateClusterSchema = async(kubeApiUrl, schemaToken) => {
@@ -456,5 +434,17 @@ const connectSub = (wsUrl, emitterId, clientId, query, connectionParams) => {
     console.log('Connection failed...', error)
   }
 }
+
+// BASE SERVER STARTUP
+const serverStart = () => {
+  let PORT = `${process.env.SERVER_PORT}`;
+  if (!process.env.SERVER_PORT) {
+    PORT = 8080;
+  }
+  wsServer.on('request', app);
+  wsServer.listen(PORT, () => {
+    console.log(`🚀 Ws server Listening on port ${wsServer.address().port}`)
+  }) 
+};
 
 serverStart();
